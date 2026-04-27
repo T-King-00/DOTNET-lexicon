7906fbbdab126251a56ddf249b2bc0da0e87eef0
T-King-00 <tonyshereen@outlook.com>
2026-04-27 10:57:06 +0200

    feat: ShoppingCart Sln, project are added.

---
+++ ShoppingCart/ShoppingCart/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
